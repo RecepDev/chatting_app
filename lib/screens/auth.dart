a645b7049c6115a56ae5b9efc9ccdd9e282359d9
1crazy571 <rdkimmbenn57>
2023-10-22 12:23:06 +0300

    s

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 
-import 'package:chatting_app/widgets/user_image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
+
+import 'package:chatting_app/widgets/user_image_picker.dart';
 
 final _firebase = FirebaseAuth.instance;
 
